be1c4ad414a0a67a9033bf122052f5f824f1da20
Raphael Beltrand <raphael.beltrand@epitech.eu>
2018-04-09 14:36:58 +0200

    clean up

--- dataCollector.js
+++ dataCollector.js
@@ -4,14 +4,19 @@ const DataCollector = require('./lib/hooksApi');
 
 const parseArgs = require('minimist');
 const arguments = parseArgs(process.argv);
+const readline = require('readline');
 
 (async() => {
 	const collector = new DataCollector();
 
 	let res;
+
 	switch (arguments.a) {
-		case "WeatherToday":
-			res = await collector.getWeatherInCity(arguments.c, arguments.d, arguments.e);
+		case "WeatherOnDay":
+			res = await collector.getWeatherInCityByDate(arguments.c, arguments.d, arguments.e);
+			break;
+		case "WeatherNow":
+			res = await collector.getWeatherInCityNow(arguments.c, arguments.d);
 			break;
 		case "NextRainyDays":
 			res = await collector.getNextRainyDaysInCity(arguments.c, arguments.d);
@@ -19,32 +24,12 @@ const arguments = parseArgs(process.argv);
 		case "NextSnowyDays":
 			res = await collector.getNextSnowyDaysInCity(arguments.c, arguments.d);
 			break;
-		case "Countries":
-			res = await collector.getCountriesList(arguments.c, arguments.f, arguments.s);
-			break;
 		case "Soccer":
 			res = await collector.getSoccerGames(arguments.d);
-			break;
-		case "MLB":
-			res = await collector.getMLBGames(arguments.c, arguments.f, arguments.s);
-			break;
-		case "NFL":
-			res = await collector.getNFLGames(arguments.c, arguments.f, arguments.s);
 			break;
 
 	}
 
-//		let leagues = [];
-//		res.items.forEach(function(e) {
-//			if (!leagues.includes(e.competition))
-//				leagues.push(e.competition);
-////			console.log(e);
-////			console.log("\n\n====================================================\n\n");
-//
-//		});
-//		leagues.sort();
-//		console.log(leagues);
-////		console.log(res.items[0].city.country.name);
-		console.log(res);
+	console.log(res);
 
 })();
